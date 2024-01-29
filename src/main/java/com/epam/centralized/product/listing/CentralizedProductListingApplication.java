47903950ea6a5d4e35fea740aebc62747f8adf8f
Giorgi Latsabidze <giolatsa@gmail.com>
2024-01-29 11:46:56 +0400

    Inital starter project

---
+++ src/main/java/com/epam/centralized/product/listing/CentralizedProductListingApplication.java
@@ -0,0 +1,13 @@
+package com.epam.centralized.product.listing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CentralizedProductListingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CentralizedProductListingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/epam/centralized/product/listing/CentralizedProductListingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epam.centralized.product.listing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CentralizedProductListingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
