a712dd53eb7a6d167e5cd260caf194710100b439
Giorgi Latsabidze <giolatsa@gmail.com>
2024-02-07 18:01:49 +0400

    adds function to fetch products by categoryName

--- src/main/java/com/epam/centralized/product/listing/service/ProductService.java
+++ src/main/java/com/epam/centralized/product/listing/service/ProductService.java
@@ -1,10 +1,11 @@
 package com.epam.centralized.product.listing.service;
 
 import com.epam.centralized.product.listing.model.Product;
-
 import java.util.List;
 
 public interface ProductService {
 
-    List<Product> getAllProducts();
+  List<Product> getAllProducts();
+
+  List<Product> getProductsByCategory(String categoryName);
 }
--- src/main/java/com/epam/centralized/product/listing/service/ProductServiceImpl.java
+++ src/main/java/com/epam/centralized/product/listing/service/ProductServiceImpl.java
@@ -2,22 +2,25 @@ package com.epam.centralized.product.listing.service;
 
 import com.epam.centralized.product.listing.model.Product;
 import com.epam.centralized.product.listing.repository.ProductRepository;
-import org.springframework.stereotype.Service;
-
 import java.util.List;
-
+import org.springframework.stereotype.Service;
 
 @Service
 public class ProductServiceImpl implements ProductService {
 
-    private final ProductRepository productRepository;
+  private final ProductRepository productRepository;
 
-    public ProductServiceImpl(ProductRepository productRepository) {
-        this.productRepository = productRepository;
-    }
+  public ProductServiceImpl(ProductRepository productRepository) {
+    this.productRepository = productRepository;
+  }
 
-    @Override
-    public List<Product> getAllProducts() {
-        return productRepository.findAll();
-    }
+  @Override
+  public List<Product> getAllProducts() {
+    return productRepository.findAll();
+  }
+
+  @Override
+  public List<Product> getProductsByCategory(String categoryName) {
+    return productRepository.findByProductCategoryCategoryName(categoryName);
+  }
 }
