f5b353d5b7f4520e40e9fc01eae4a736a0ff0551
Giorgi Latsabidze <giolatsa@gmail.com>
2024-02-07 23:59:08 +0400

    Adds cart controller to handle cart functionality

--- src/main/java/com/epam/centralized/product/listing/controller/CartController.java
+++ src/main/java/com/epam/centralized/product/listing/controller/CartController.java
@@ -1,12 +1,17 @@
 package com.epam.centralized.product.listing.controller;
 
+import com.epam.centralized.product.listing.model.Product;
 import com.epam.centralized.product.listing.service.CartService;
+import com.epam.centralized.product.listing.service.ProductService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.security.Principal;
+import java.util.List;
 
 @Controller
 @RequestMapping("/cart")
@@ -14,15 +19,39 @@ public class CartController {
 
     private final CartService cartService;
 
-    public CartController(CartService cartService) {
+    private final ProductService productService;
+
+    public CartController(CartService cartService, ProductService productService) {
         this.cartService = cartService;
+        this.productService = productService;
+    }
+
+    @GetMapping
+    public String cart(Model model, Principal principal) {
+        String username = principal.getName();
+
+        List<Product> products = productService.getProductsInCart(username);
+
+        Double total = products.stream().mapToDouble(Product::getPrice).sum();
+
+        model.addAttribute("products", products);
+        model.addAttribute("total", total);
+        model.addAttribute("productCount", products.size());
+        return "cart";
     }
 
     @PostMapping("/add")
     public String addToCart(@RequestParam("productId") Long productId, Principal principal) {
         String username = principal.getName(); // Get logged-in username
         cartService.addProductToCart(username, productId);
-        return "redirect:/home"; // Redirect to a page of your choice
+        return "redirect:/home";
+    }
+
+    @PostMapping("/remove")
+    public String removeFromCart(@RequestParam("productId") Long productId, Principal principal) {
+        String username = principal.getName(); // Get logged-in username
+        cartService.removeProductFromCart(username, productId);
+        return "redirect:/cart";
     }
 
 
