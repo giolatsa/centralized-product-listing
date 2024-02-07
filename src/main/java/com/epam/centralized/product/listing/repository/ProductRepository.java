1bdf6b6c09d912be0d7ed4aa3e8f611d6a5acc75
Giorgi Latsabidze <giolatsa@gmail.com>
2024-02-07 18:01:37 +0400

    adds function to fetch products by categoryName

--- src/main/java/com/epam/centralized/product/listing/repository/ProductRepository.java
+++ src/main/java/com/epam/centralized/product/listing/repository/ProductRepository.java
@@ -1,6 +1,9 @@
 package com.epam.centralized.product.listing.repository;
 
 import com.epam.centralized.product.listing.model.Product;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProductRepository extends JpaRepository<Product, Long> {}
+public interface ProductRepository extends JpaRepository<Product, Long> {
+  List<Product> findByProductCategoryCategoryName(String categoryName);
+}
