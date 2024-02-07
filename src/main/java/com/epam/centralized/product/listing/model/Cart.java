fb72a52895a36cc8cd2fd4c456dbd0d9ee9f4d54
Giorgi Latsabidze <giolatsa@gmail.com>
2024-02-07 23:08:22 +0400

    adds createDate and updateDate to cart model

--- src/main/java/com/epam/centralized/product/listing/model/Cart.java
+++ src/main/java/com/epam/centralized/product/listing/model/Cart.java
@@ -2,12 +2,21 @@ package com.epam.centralized.product.listing.model;
 
 import com.epam.centralized.product.listing.model.enums.CartStatus;
 import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
 import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Entity
 @Table(name = "cart")
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Cart {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -23,4 +32,8 @@ public class Cart {
   private List<Product> products;
 
   private CartStatus cartStatus;
+
+  private LocalDateTime createDate;
+
+    private LocalDateTime updateDate;
 }
